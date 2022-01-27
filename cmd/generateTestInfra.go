7eadf4a39617b02de60fe605607ae9fe6ab24cf8
Prakash Sanker <praks5432@gmail.com>
2022-01-27 11:43:56 +0530

    1. Move functions into a package 2. Create deploy.go cmd

--- cmd/generateTestInfra.go
+++ cmd/generateTestInfra.go
@@ -57,14 +57,4 @@ var generateTestInfra = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(generateTestInfra)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// generateTestInfraCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// generateTestInfraCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
